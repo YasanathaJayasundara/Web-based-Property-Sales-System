b70221241d091c4122b0de5112f08fc2ab53984a
Yasantha Jayasundara <yasanthavjayasundara@gmail.com>
2026-08-31 17:33:26 +0530

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/property/app/PropertySalesSpringApplication.java
@@ -0,0 +1,12 @@
+package com.realestate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateSpringApplication.class, args);
+    }
+}
+
