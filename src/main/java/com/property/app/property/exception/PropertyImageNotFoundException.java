1e9abc55a2135a90b0691bdaaa898f86620e7f5d
Yasantha Jayasundara <yasanthavjayasundara@gmail.com>
2026-09-12 18:13:42 +0530

    feat(property): add image not found exception

--- src/main/java/com/property/app/property/exception/PropertyImageNotFoundException.java
+++ src/main/java/com/property/app/property/exception/PropertyImageNotFoundException.java
@@ -1,7 +1,16 @@
 package com.property.app.property.exception;
 
-public class PropertyImageNotFoundException extends RuntimeException {
-  public PropertyImageNotFoundException(String message) {
-    super(message);
-  }
+public class PropertyImageNotFoundException
+        extends RuntimeException {
+
+    public PropertyImageNotFoundException(
+            Long propertyId,
+            Long imageId
+    ) {
+        super(
+                "Image " + imageId
+                        + " was not found for property "
+                        + propertyId
+        );
+    }
 }
