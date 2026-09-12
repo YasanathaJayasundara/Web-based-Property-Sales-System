50876d9692ddf05f2c1ddf72a6a9d6560934f913
Yasantha Jayasundara <yasanthavjayasundara@gmail.com>
2026-09-12 18:13:36 +0530

    feat(property): add invalid property image exception

--- src/main/java/com/property/app/property/exception/InvalidPropertyImageException.java
+++ src/main/java/com/property/app/property/exception/InvalidPropertyImageException.java
@@ -1,7 +1,9 @@
 package com.property.app.property.exception;
 
-public class InvalidPropertyImageException extends RuntimeException {
-  public InvalidPropertyImageException(String message) {
-    super(message);
-  }
+public class InvalidPropertyImageException
+        extends RuntimeException {
+
+    public InvalidPropertyImageException(String message) {
+        super(message);
+    }
 }
